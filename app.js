5439fa1febdabd59d0d2a6c1353a40445a094eb9
colecrum26 <cwcrumrine@gmail.com>
2023-03-16 13:54:18 -0500

    working on exercise one

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const arr1 = [2, 22, 12, 17, 18, 39, 129];
+let sum = 0;
+function arraySum(sum){
+    for (let i = 0; i < arr1.length; i++){
+        sum += arr1[i];
+        console.log(sum)
+    }
+    return sum;
+}
+
+arraySum(sum);
+
+arr1.forEach((sum, i, arr1) => {
+    sum += arr1[i];
+    console.log(sum);
+})
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
