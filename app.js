3c190dc9ccf4fb84c5af5588f96c6f654f507892
colecrum26 <cwcrumrine@gmail.com>
2023-03-17 11:17:31 -0500

    worked with split

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ let sum = 0;
 function arraySum(sum) {
   for (let i = 0; i < arr1.length; i++) {
     sum += arr1[i];
-    console.log(sum);
+    // console.log(sum);
   }
   return sum;
 }
 
-arraySum(sum);
+// arraySum(sum);
 
 // HIGHER ORDER FUNCTIONS
 // arr1.forEach((sum) => {
@@ -32,15 +32,55 @@ console.log("EXERCISE 2:\n==========\n");
 //   (book.readCount = "one");
 
 const book = {
-title: "The Institute",
-author: "Stephen King",
-noOfPages: 557,
-readCount: "one"
-}
+  title: "The Institute",
+  author: "Stephen King",
+  noOfPages: 557,
+  readCount: "one",
+};
 
 book.info = function () {
   return `${this.title} was written by ${this.author} and has ${this.noOfPages} pages. Hayden has read ${this.title}
 ${this.readCount} time(s).`;
 };
 
-console.log(book.info());
+// console.log(book.info());
+
+// Exercise 3
+console.log("EXERCISE 3:\n==========\n");
+
+// My Attempt
+// let sentence = "The quick brown fox jumps over the lazy dog";
+
+// let wordArray = sentence.split(" ");
+// console.log(wordArray);
+
+// for (let i = wordArray.length - 1; i > 0; i--) {
+//   let result = [];
+//   let charArray = wordArray[i].split("");
+//   let revWord = charArray.join("");
+//   result.unshift(revWord[i]);
+//   return result.join("");
+// }
+// console.log(result);
+
+
+// In Class Attempt
+let sentence = "The quick brown fox jumps over the lazy dog";
+
+function reverseWord(sentence) {
+  let result = [];
+  let words = sentence.split(" ");
+  console.log("these are the words split", words);
+  for (let i = 0; i < words.length; i++){
+    let letters = words[i].split('');
+    console.log("these are the letters split", letters);
+    for (let j = letters.length -1; j >= 0; j--) {
+      result.push(letters[j]);
+      console.log("this is after push for result", result)
+    }
+    result.push(" ");
+  }
+  return result.join('');
+}
+
+console.log(reverseWord(sentence));
